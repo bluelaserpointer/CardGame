5b48fde90c360882a2aee815993ff4090d71abcf
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-07-15 08:33:04 +0800

    feat: added simple login feature for client

---
+++ Surviver_client/app/src/main/java/com/example/myapplicationtest1/utils/Utils.java
@@ -0,0 +1,32 @@
+package com.example.myapplicationtest1.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Utils {
+    public static boolean saveUserInfo(Context context, String userName,
+                                       String password) {
+        SharedPreferences sp = context.getSharedPreferences("data", Context.MODE_PRIVATE);//数据自己可用
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putString("userName", userName);
+        edit.putString("password", password);
+        edit.commit();
+        return true;//存储成功
+    }
+
+    // 从data.xml中获取QQ号码和密码
+    public static Map<String, String> getUserInfo(Context context) {
+        SharedPreferences sp = context.getSharedPreferences("data", Context.MODE_PRIVATE);
+
+        String userName = sp.getString("userName", null);
+        String password = sp.getString("password", null);
+        Map<String,String> userMap = new HashMap<String, String>();
+        userMap.put("userName", userName);
+        userMap.put("password", password);
+
+        return userMap;
+    }
+}
