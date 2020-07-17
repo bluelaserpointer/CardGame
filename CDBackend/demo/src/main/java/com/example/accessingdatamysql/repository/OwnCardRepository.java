df88e43ec7b858cac9c3ab4d21e191049ab6531a
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-07-17 15:56:12 +0800

    fix: fixed ownCard bug for saving entities

--- CDBackend/demo/src/main/java/com/example/accessingdatamysql/repository/OwnCardRepository.java
+++ CDBackend/demo/src/main/java/com/example/accessingdatamysql/repository/OwnCardRepository.java
@@ -7,19 +7,23 @@ import org.springframework.data.jpa.repository.Modifying;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.query.Param;
 
 public interface OwnCardRepository extends JpaRepository<OwnCard, Integer> {
-    @Modifying
-    @Transactional
-    void deleteOwnCardByUserIdEqualsAndCardIdEquals(Integer userId, Integer cardId);
+    Optional<OwnCard> findOwnCardByUserIdEqualsAndCardIdEquals(Integer userId, Integer cardId);
 
-    @Query(value = "SELECT u from OwnCards u where u.userId = ?1", nativeQuery = true)
+    @Query(value = "SELECT u from OwnCard u where u.userId = ?1", nativeQuery = true)
     List<OwnCard> findByUserId(Integer userId);
 
     // 这里还没有测试过可能会出现bug
-    @Query(value = "SELECT u from OwnCards u where u.userId = ?1 and u.cardId = ?2", nativeQuery = true)
+    @Query(value = "SELECT u from OwnCard u where u.userId = ?1 and u.cardId = ?2", nativeQuery = true)
     OwnCard findByUserIdCardId(Integer userId, Integer cardId);
+
+    @Modifying
+    @Transactional
+    void deleteOwnCardByUserIdEqualsAndCardIdEquals(Integer userId, Integer cardId);
 
     @Transactional
     @Modifying
