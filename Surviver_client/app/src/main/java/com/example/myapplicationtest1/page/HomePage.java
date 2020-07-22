ce709e1674ec718366e1cd88036ebe044fabc098
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2020-07-22 15:10:20 +0800

    feat(client): 主页面显示用户头像/名字/等级/经验值，头像还没有更改功能

--- Surviver_client/app/src/main/java/com/example/myapplicationtest1/page/HomePage.java
+++ Surviver_client/app/src/main/java/com/example/myapplicationtest1/page/HomePage.java
@@ -1,12 +1,14 @@
 package com.example.myapplicationtest1.page;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myapplicationtest1.R;
 import com.example.myapplicationtest1.game.core.GHQ;
 import com.example.myapplicationtest1.utils.Utils;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomePage extends Page {
     @Override
@@ -20,6 +22,14 @@ public class HomePage extends Page {
         super.setJump(R.id.toAnnounce_button, AnnouncePage.class);
         super.setJump(R.id.toBag_button, TeamPage.class);
         GHQ.setResource(getResources());
-        Utils.setUserTopBarInfo(this);
+        try {
+            final JSONObject userInfo = Utils.getUserInfo();
+            Utils.setUserTopBarInfo(this, userInfo);
+            ((TextView)findViewById(R.id.userName)).setText(Utils.getUserName());
+            ((TextView)findViewById(R.id.userLevel)).setText("LV: " + userInfo.getInt("level"));
+            ((TextView)findViewById(R.id.userExp)).setText("exp: " + userInfo.getInt("curExpPoint"));
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
