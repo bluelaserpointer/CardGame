71a766af1109409b05450ba6a041734b77922947
Naomi <41462684+naomixie@users.noreply.github.com>
2020-07-29 00:10:57 +0800

    Update：progress

--- CDBackend/demo/src/test/java/com/example/accessingdatamysql/GeneralTests/Controller/ChapterControllerTest.java
+++ CDBackend/demo/src/test/java/com/example/accessingdatamysql/GeneralTests/Controller/ChapterControllerTest.java
@@ -1,164 +1,186 @@
-// package com.example.accessingdatamysql.GeneralTests.Controller;
+package com.example.accessingdatamysql.GeneralTests.Controller;
 
 // import static org.assertj.core.api.Assertions.assertThat;
 
+import com.alibaba.fastjson.JSON;
 // import com.example.accessingdatamysql.controller.ChapterController;
+import com.example.accessingdatamysql.entity.AuthRequest;
 
-// import org.junit.Before;
-// import org.junit.Test;
-// import org.junit.jupiter.api.AfterAll;
-// import org.junit.jupiter.api.AfterEach;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.runner.RunWith;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import
-// org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.http.MediaType;
-// import org.springframework.test.annotation.Rollback;
-// import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-// import org.springframework.test.web.servlet.MockMvc;
-// import org.springframework.test.web.servlet.MvcResult;
-// import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-// import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-// import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-// import org.springframework.transaction.annotation.Transactional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
-// import static
-// org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-// @RunWith(SpringJUnit4ClassRunner.class)
-// @SpringBootTest
-// @AutoConfigureMockMvc
-// public class ChapterControllerTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ChapterControllerTest {
 
-// @Test
-// @DisplayName("File: ChapterController Method: contextLoads")
-// public void contextLoads() {
-// assertThat(ChapterController).isNotNull();
-// }
+    @Autowired
+    private WebApplicationContext context;
 
-// private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-// @Autowired
-// private ChapterController ChapterController;
+    private String TOKEN;
 
-// @Before
-// public void setUp() {
-// mockMvc = MockMvcBuilders.standaloneSetup(ChapterController).build();
-// }
+    public String getTOKEN() throws Exception {
+        System.out.println(TOKEN);
+        AuthRequest user = new AuthRequest();
+        user.setPassword("postTest");
+        user.setUserName("postTest");
 
-// @AfterEach
-// void tearDown() {
+        String body = JSON.toJSONString(user);
+        System.out.println(body);
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("http://localhost:8080/user/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).content(body))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        TOKEN = result.getResponse().getContentAsString();
+        TOKEN = "Bearer " + TOKEN;
+        System.out.println(TOKEN);
+        return TOKEN;
+    }
 
-// }
+    @Test
+    @DisplayName("File: ChapterController Method: contextLoads")
+    public void contextLoads() {
+    }
 
-// @AfterAll
-// static void tearDownAll() {
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
 
-// }
+    @AfterEach
+    void tearDown() {
 
-// // @Test
-// // @Transactional
-// // @Rollback(value = true)
-// // @DisplayName("File: ChapterController Method: updateChapterPhaseStrategy")
-// // public void updateChapterPhaseStrategy() throws Exception {
-// // MvcResult result = mockMvc.perform(get(
-// //
-// "/chapter/updateChapterPhaseStrategy?chapterId=1&phaseId=1&phaseData=updateChapterPhaseStrategyTest")
-// // .contentType(MediaType.APPLICATION_JSON_VALUE))
-// //
-// .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
-// // .andReturn();
-// // System.out.println(result.getResponse().getContentAsString());
-// // }
+    }
 
-// // @Test
-// // @Transactional
-// // @Rollback(value = true)
-// // @DisplayName("File: ChapterController Method: updateChapterPhaseAwards")
-// // public void updateChapterPhaseAwards() throws Exception {
-// // MvcResult result = mockMvc.perform(get(
-// //
-// "/chapter/updateChapterPhaseAwards?chapterId=1&phaseId=1&awardItems=updateChapterPhaseAwardsTest&awardCards=updateChapterPhaseAwardsTest")
-// // .contentType(MediaType.APPLICATION_JSON_VALUE))
-// //
-// .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
-// // .andReturn();
-// // System.out.println(result.getResponse().getContentAsString());
-// // }
+    @AfterAll
+    static void tearDownAll() {
+    }
 
-// // @Test
-// // @Transactional
-// // @Rollback(value = true)
-// // @DisplayName("File: ChapterController Method: updateChapterAwards")
-// // public void updateChapterAwards() throws Exception {
-// // MvcResult result = mockMvc.perform(get(
-// //
-// "/chapter/updateChapterAwards?chapterId=1&awardItems=updateChapterAwardsTest&awardCards=updateChapterAwardsTest")
-// // .contentType(MediaType.APPLICATION_JSON_VALUE))
-// //
-// .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
-// // .andReturn();
-// // System.out.println(result.getResponse().getContentAsString());
-// // }
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    @DisplayName("File: ChapterController Method: updateChapterPhaseStrategy")
+    public void updateChapterPhaseStrategy() throws Exception {
+        String token = getTOKEN();
+        MvcResult result = mockMvc.perform(get(
 
-// // @Test
-// // @Transactional
-// // @Rollback(value = true)
-// // @DisplayName("File: ChapterController Method: getAllChapters")
-// // public void getAllChapters() throws Exception {
-// // MvcResult result = mockMvc
-// //
-// .perform(get("/chapter/getAllChapters").contentType(MediaType.APPLICATION_JSON_VALUE))
-// //
-// .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
-// // .andReturn();
-// // System.out.println(result.getResponse().getContentAsString());
-// // }
+                "/chapter/updateChapterPhaseStrategy?chapterId=1&phaseId=1&phaseData=updateChapterPhaseStrategyTest")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).header("Authorization", token))
 
-// // @Test
-// // @Transactional
-// // @Rollback(value = true)
-// // @DisplayName("File: ChapterController Method:
-// // getChapterDetailsByChapterAndByPhase")
-// // public void getChapterDetailsByChapterAndByPhase() throws Exception {
-// // MvcResult result = mockMvc
-// //
-// .perform(get("/chapter/getChapterDetailsByChapterAndByPhase?chapterId=1&phaseId=1")
-// // .contentType(MediaType.APPLICATION_JSON_VALUE))
-// //
-// .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
-// // .andReturn();
-// // System.out.println(result.getResponse().getContentAsString());
-// // }
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
 
-// // @Test
-// // @Transactional
-// // @Rollback(value = true)
-// // @DisplayName("File: ChapterController Method: getChapterDetailsByChapter")
-// // public void getChapterDetailsByChapter() throws Exception {
-// // MvcResult result = mockMvc
-// // .perform(get("/Chapter/getChapterDetailsByChapter?chapterId=1&phaseId=1")
-// // .contentType(MediaType.APPLICATION_JSON_VALUE))
-// //
-// .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
-// // .andReturn();
-// // System.out.println(result.getResponse().getContentAsString());
-// // }
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    @DisplayName("File: ChapterController Method: updateChapterPhaseAwards")
+    public void updateChapterPhaseAwards() throws Exception {
+        String token = getTOKEN();
+        MvcResult result = mockMvc.perform(get(
 
-// // @Test
-// // @Transactional
-// // @Rollback(value = true)
-// // @DisplayName("File: ChapterController Method: getAllChapterDetails")
-// // public void getAllChapterDetails() throws Exception {
-// // MvcResult result = mockMvc
-// // .perform(get("/Chapter/getAllChapterDetails")
-// // .contentType(MediaType.APPLICATION_JSON_VALUE))
-// //
-// .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
-// // .andReturn();
-// // System.out.println(result.getResponse().getContentAsString());
-// // }
+                "/chapter/updateChapterPhaseAwards?chapterId=1&phaseId=1&awardItems=updateChapterPhaseAwardsTest&awardCards=updateChapterPhaseAwardsTest")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).header("Authorization", token))
 
-// }
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    @DisplayName("File: ChapterController Method: updateChapterAwards")
+    public void updateChapterAwards() throws Exception {
+        String token = getTOKEN();
+        MvcResult result = mockMvc.perform(get(
+
+                "/chapter/updateChapterAwards?chapterId=1&awardItems=updateChapterAwardsTest&awardCards=updateChapterAwardsTest")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).header("Authorization", token))
+
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    @DisplayName("File: ChapterController Method: getAllChapters")
+    public void getAllChapters() throws Exception {
+        String token = getTOKEN();
+        MvcResult result = mockMvc
+
+                .perform(get("/chapter/getAllChapters").contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .header("Authorization", token))
+
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    @DisplayName("File: ChapterController Method: getChapterDetailsByChapterAndByPhase")
+    public void getChapterDetailsByChapterAndByPhase() throws Exception {
+        String token = getTOKEN();
+        MvcResult result = mockMvc
+
+                .perform(get("/chapter/getChapterDetailsByChapterAndByPhase?chapterId=1&phaseId=1")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).header("Authorization", token))
+
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    @DisplayName("File: ChapterController Method: getChapterDetailsByChapter")
+    public void getChapterDetailsByChapter() throws Exception {
+        String token = getTOKEN();
+        MvcResult result = mockMvc
+                .perform(get("/Chapter/getChapterDetailsByChapter?chapterId=1&phaseId=1")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).header("Authorization", token))
+
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    @DisplayName("File: ChapterController Method: getAllChapterDetails")
+    public void getAllChapterDetails() throws Exception {
+        String token = getTOKEN();
+        MvcResult result = mockMvc
+                .perform(get("/Chapter/getAllChapterDetails").contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .header("Authorization", token))
+
+                .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+}
