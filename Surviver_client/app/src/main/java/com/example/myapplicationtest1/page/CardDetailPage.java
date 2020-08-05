78fb7b451e91ea3e6e01c84ec23c7cd3701b7300
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2020-08-05 18:41:52 +0800

    wip(client): 强化点数分配功能-已分配点数显示

--- Surviver_client/app/src/main/java/com/example/myapplicationtest1/page/CardDetailPage.java
+++ Surviver_client/app/src/main/java/com/example/myapplicationtest1/page/CardDetailPage.java
@@ -18,16 +18,22 @@ public class CardDetailPage extends Page {
         super.setJump(R.id.return_button, CardStoragePage.class);
         //setCardStatus
         ((ImageView)findViewById(R.id.cardIcon)).setImageResource(selectingOwnCard.card.drawableId);
-        ((TextView)findViewById(R.id.cardRarity)).setText(selectingOwnCard.card.rarity);
+        ((TextView)findViewById(R.id.cardRarity)).setText(selectingOwnCard.card.rarity + " ");
         ((TextView)findViewById(R.id.cardName)).setText(selectingOwnCard.card.cardName);
         ((TextView)findViewById(R.id.cardLevel)).setText(selectingOwnCard.cardLevel + "/" + selectingOwnCard.cardLevelLimit);
         ((TextView)findViewById(R.id.cardExp)).setText(selectingOwnCard.cardCurExp + "/" + 100);
         ((TextView)findViewById(R.id.cardSP)).setText(selectingOwnCard.leftPoints + "/" + selectingOwnCard.enhancePoint);
         ((TextView)findViewById(R.id.cardHP)).setText(String.valueOf(selectingOwnCard.healthPoint()));
-        ((TextView)findViewById(R.id.cardATK)).setText(String.valueOf(selectingOwnCard.attackRange()));
+        ((TextView)findViewById(R.id.cardHPEnhanced)).setText(String.valueOf(selectingOwnCard.enhanceHP));
+        ((TextView)findViewById(R.id.cardATK)).setText(String.valueOf(selectingOwnCard.attack()));
+        ((TextView)findViewById(R.id.cardATKEnhanced)).setText(String.valueOf(selectingOwnCard.enhanceAttack));
         ((TextView)findViewById(R.id.cardDEF)).setText(String.valueOf(selectingOwnCard.defense()));
+        ((TextView)findViewById(R.id.cardDEFEnhanced)).setText(String.valueOf(selectingOwnCard.enhanceDefense));
         ((TextView)findViewById(R.id.cardATKRange)).setText(String.valueOf(selectingOwnCard.attackRange()));
+        ((TextView)findViewById(R.id.cardATKRangeEnhanced)).setText(String.valueOf(selectingOwnCard.enhanceAttackRange));
         ((TextView)findViewById(R.id.cardCD)).setText(String.valueOf(selectingOwnCard.cd()));
+        ((TextView)findViewById(R.id.cardCDEnhanced)).setText(String.valueOf(selectingOwnCard.enhanceCD));
+        ((TextView)findViewById(R.id.cardDescription)).setText(String.valueOf(selectingOwnCard.card.description));
         super.setTouchEvent(R.id.addHP, () -> { if(drawSp()) ++hpUpg; });
         super.setTouchEvent(R.id.addATK, () -> { if(drawSp()) ++atkUpg; });
         super.setTouchEvent(R.id.addDEF, () -> { if(drawSp()) ++defUpg; });
