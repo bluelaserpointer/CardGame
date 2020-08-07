b5e2a2ba30093f31076084e5ba4534ab586ef470
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-08-07 14:15:32 +0800

    feat: added schedules and all the apis for data graphs

--- CDBackend/demo/src/main/java/com/example/accessingdatamysql/Schedule/DataCollectionSchedule.java
+++ CDBackend/demo/src/main/java/com/example/accessingdatamysql/Schedule/DataCollectionSchedule.java
@@ -1,6 +1,7 @@
 package com.example.accessingdatamysql.Schedule;
 
 import com.example.accessingdatamysql.service.OnlineCountRecordService;
+import com.example.accessingdatamysql.service.UserLoginRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,18 @@ import org.springframework.stereotype.Component;
 public class DataCollectionSchedule {
     @Autowired
     private OnlineCountRecordService onlineCountRecordService;
+    @Autowired
+    private UserLoginRecordService userLoginRecordService;
+
     //3.添加定时任务
-//    @Scheduled(cron = "0/30 * * ? * * *")
     //或直接指定时间间隔，例如：5秒
-    @Scheduled(fixedRate=30000)
+    @Scheduled(fixedRate = 30000)
     private void saveCount() {
         onlineCountRecordService.saveCount();
+    }
+
+    @Scheduled(fixedRate = 30000)
+    private void calculateLogout() {
+        userLoginRecordService.calculateLogout();
     }
 }
