bc9f4a5d1986fa75e14d016d29217fe76c883535
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-08-07 14:13:17 +0800

    feat: added necessary classes for crashReport graphs

--- CDBackend/demo/src/main/java/com/example/accessingdatamysql/dao/CrashReportsDao.java
+++ CDBackend/demo/src/main/java/com/example/accessingdatamysql/dao/CrashReportsDao.java
@@ -2,7 +2,11 @@ package com.example.accessingdatamysql.dao;
 
 import com.example.accessingdatamysql.entity.CrashReports;
 
+import java.util.List;
+
 public interface CrashReportsDao {
     CrashReports getOne(Integer id);
     void addNew(String reportsContent);
+    List<CrashReports> getCrashReportsWithinHalfYear();
+    List<CrashReports> getCrashReportsWithinOneDay();
 }
--- CDBackend/demo/src/main/java/com/example/accessingdatamysql/repository/CrashReportsRepository.java
+++ CDBackend/demo/src/main/java/com/example/accessingdatamysql/repository/CrashReportsRepository.java
@@ -2,6 +2,17 @@ package com.example.accessingdatamysql.repository;
 
 import com.example.accessingdatamysql.entity.CrashReports;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface CrashReportsRepository extends JpaRepository<CrashReports, Integer> {
+    @Transactional
+    @Query(value = "select * from crash_reports where cast(substr(record_time, 15, 2) as signed) <= 30 and record_time > DATE_SUB(NOW(),INTERVAL 6 MONTH) order by record_time", nativeQuery = true)
+    List<CrashReports> findCrashReportsWithinHalfYear();
+
+    @Transactional
+    @Query(value = "select * from crash_reports where record_time > DATE_SUB(NOW(),INTERVAL 1 DAY) order by record_time", nativeQuery = true)
+    List<CrashReports> findCrashReportsWithinOneDay();
 }
