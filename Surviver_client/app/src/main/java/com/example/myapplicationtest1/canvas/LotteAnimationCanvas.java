4b0bd52e4893cb5492aabe8063c644de022ad0d3
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2020-08-09 07:50:02 +0800

    improvement(clinet): 抽卡页面时的抽卡资源fetch省去，换为从缓冲获取

--- Surviver_client/app/src/main/java/com/example/myapplicationtest1/canvas/LotteAnimationCanvas.java
+++ Surviver_client/app/src/main/java/com/example/myapplicationtest1/canvas/LotteAnimationCanvas.java
@@ -23,7 +23,6 @@ public class LotteAnimationCanvas extends MyCanvas {
     //paints
     private Paint whitePaint, chiPaint, engPaint, matPaint;
     //data
-    public static int stockCHI, stockENG, stockMAT;
     public static int betCHI, betENG, betMAT;
     //animations
     private int animationFrame;
@@ -55,14 +54,6 @@ public class LotteAnimationCanvas extends MyCanvas {
         matPaint = new Paint();
         matPaint.setColor(Color.BLUE);
         matPaint.setTextSize(40);
-        try {
-            JSONObject data = new JSONObject(HttpClient.doGetShort(Urls.getUser()));
-            stockCHI = data.getInt("chiKnowledge");
-            stockENG = data.getInt("engKnowledge");
-            stockMAT = data.getInt("mathKnowledge");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
     @Override
     protected void onDraw(Canvas canvas) {
@@ -70,9 +61,9 @@ public class LotteAnimationCanvas extends MyCanvas {
         ++animationFrame;
         GHQ.setTargetCanvas(canvas);
         canvas.drawRect(0, 0, getWidth(), getHeight(), whitePaint);
-        GHQ.drawStringGHQ("语文知识:" + betCHI + "/" + stockCHI, 50, 50, chiPaint);
-        GHQ.drawStringGHQ("英语知识:" + betENG + "/" + stockENG, 50, 100, engPaint);
-        GHQ.drawStringGHQ("数学知识:" + betMAT + "/" + stockMAT, 50, 150, matPaint);
+        GHQ.drawStringGHQ("语文知识:" + betCHI + "/" + Cache.chiKnowledge, 50, 50, chiPaint);
+        GHQ.drawStringGHQ("英语知识:" + betENG + "/" + Cache.mathKnowledge, 50, 100, engPaint);
+        GHQ.drawStringGHQ("数学知识:" + betMAT + "/" + Cache.engKnowledge, 50, 150, matPaint);
         canvas.drawRect(getWidth()/2 - 100, getHeight()/2 - 100, getWidth()/2 + 100, getHeight()/2 + 100, GHQ.generatePaint(Color.GREEN));
         if(lastDrawnCard != null) {
             lastDrawnCardImage.dotPaint(getWidth()/2, getHeight() - lastDrawnCardImage.height()/2);
@@ -87,24 +78,27 @@ public class LotteAnimationCanvas extends MyCanvas {
             try {
                 drawnCardJson = new JSONObject(HttpClient.doGetShort(Urls.drawCard(betCHI, betMAT, betENG)));
                 lastDrawnCard = Cache.cards.get(drawnCardJson.getInt("drawnCardId"));
+                lastDrawnCardImage = ImageFrame.create(lastDrawnCard.drawableId);
             } catch (JSONException e) {
                 e.printStackTrace();
                 return;
             }
-            lastDrawnCardImage = ImageFrame.create(lastDrawnCard.drawableId);
+            Cache.chiKnowledge -= betCHI;
+            Cache.mathKnowledge -= betMAT;
+            Cache.engKnowledge -= betENG;
             betCHI = betENG = betMAT = 0;
         }
     }
     public static void addCHI() {
-        if(betCHI < stockCHI)
+        if(betCHI < Cache.chiKnowledge)
             ++betCHI;
     }
     public static void addENG() {
-        if(betENG < stockENG)
+        if(betENG < Cache.engKnowledge)
             ++betENG;
     }
     public static void addMAT() {
-        if(betMAT < stockMAT)
+        if(betMAT < Cache.mathKnowledge)
             ++betMAT;
     }
     public static void removeCHI() {
