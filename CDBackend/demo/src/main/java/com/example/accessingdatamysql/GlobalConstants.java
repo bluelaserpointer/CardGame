f881f4fed65e1a3ca566e41763171e2f9e0091ce
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2020-08-10 16:29:14 +0800

    尝试降低pageSize

--- CDBackend/demo/src/main/java/com/example/accessingdatamysql/GlobalConstants.java
+++ CDBackend/demo/src/main/java/com/example/accessingdatamysql/GlobalConstants.java
@@ -5,6 +5,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 // This class contains all constants referenced by the project.
 public class GlobalConstants {
-    public static final Integer general_page_size = 50; // the general page size for listing entities.
+    public static final Integer general_page_size = 10; // the general page size for listing entities.
 
 }
